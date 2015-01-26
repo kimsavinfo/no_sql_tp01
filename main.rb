150632573a5fc6a88d0176ead36625b28b788d46
kimsavinfo <kimsavinfo@gmail.com>
2015-01-27 00:01:29 +0100

    [UPDATE]
    Rename

--- main.rb
+++ main.rb
@@ -3,12 +3,12 @@ require './models/worker.rb'
 urls = ["https://www.google.fr/?gws_rd=ssl", "http://ecampusbordeaux.epsi.fr/contact-info", "http://lesjoiesducode.fr/"]
 iUrl = 0
 
-jobToAdd = Worker.new
+worker = Worker.new
 
 while true do
 
-	jobToAdd.setJob("GET", urls[iUrl])
-	jobToAdd.saveJob()
+	worker.setJob("GET", urls[iUrl])
+	worker.saveJob()
 
 	iUrl = (iUrl+1).modulo(urls.length)
 
